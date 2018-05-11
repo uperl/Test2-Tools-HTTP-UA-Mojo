f8509693a041b759b96e364cdb915d4fcdc9cc3c
Graham Ollis <plicease@cpan.org>
2018-05-11 17:30:19 -0400

    initial incomplete implementation

--- lib/Test2/Tools/HTTP/UA/Mojo.pm
+++ lib/Test2/Tools/HTTP/UA/Mojo.pm
@@ -2,9 +2,90 @@ package Test2::Tools::HTTP::UA::Mojo;
 
 use strict;
 use warnings;
-use 5.008001;
+use 5.01001;
+use Mojolicious 7.00;
+use HTTP::Response;
+use HTTP::Message::PSGI;
+use parent 'Test2::Tools::HTTP::UA';
 
 # ABSTRACT: Mojo user agent wrapper for Test2::Tools::HTTP
 # VERSION
 
+=head1 SYNOPSIS
+
+ use Test2::Tools::HTTP;
+ use Mojo::UserAgent;
+ 
+ http_ua( Mojo::UserAgent->new )
+ 
+ http_request(
+   GET('http://example.test'),
+   http_response {
+     http_code 200;
+     http_response match qr/something/;
+     ...
+   }
+ );;
+ 
+ done_testing;
+
+=head1 DESCRIPTION
+
+This module is a user agent wrapper for L<Test2::Tools::HTTP> that allows you
+to use L<Mojo::UserAgent> as a user agent for testing.
+
+=cut
+
+sub instrument
+{
+  my($self) = @_;
+}
+
+sub request
+{
+  my($self, $req, %options) = @_;
+
+  require Mojo::Transaction::HTTP;
+  require Mojo::Message::Request;
+  
+  # Add the User-Agent header to the HTTP::Request
+  # so that T2::T::HTTP can see it in diagnostics
+  $req->header('User-Agent' => $self->ua->transactor->name)
+    unless $req->header('User-Agent');
+
+  my $mojo_req = Mojo::Message::Request->new;
+  $mojo_req->parse($req->to_psgi);
+
+  my $tx = Mojo::Transaction::HTTP->new(req => $mojo_req);
+  
+  $self->ua->start($tx);
+  
+  if(my $err = $tx->error)
+  {
+    my $res = HTTP::Response->new(500, 'Internal Error');
+    $res->header("Client-Warning" => $err->{message});
+    return $res;
+  }
+  else
+  {
+    return HTTP::Response->parse(
+      $tx->res->to_string
+    );
+  }
+}
+
 1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Test2::Tools::HTTP>
+
+=item L<Mojo::UserAgent>
+
+=item L<Test::Mojo>
+
+=back
+
+=cut
--- t/test2_tools_http_ua_mojo.t
+++ t/test2_tools_http_ua_mojo.t
@@ -1,6 +1,37 @@
 use Test2::V0 -no_srand => 1;
-use Test2::Tools::HTTP::UA::Mojo;
+use Test2::Tools::HTTP;
+use Test::Mojo;
+use Mojolicious::Lite;
+use HTTP::Request::Common;
 
-ok 1, 'todo';
+get '/foo' => sub {
+  my($c) = @_;
+  $c->render(text => "hello world\n");
+};
+
+my $t = Test::Mojo->new;
+
+app->log->unsubscribe('message');
+app->log->on(message => sub {
+  my($log, $level, @lines) = @_;
+  note "[$level] $_" for @lines;
+});
+
+$t->get_ok('/foo')
+  ->status_is(200)
+  ->content_is("hello world\n");
+
+http_ua($t->ua);
+
+isa_ok( http_ua, 'Mojo::UserAgent' );
+
+http_request(
+  GET('/foo'),
+  http_response {
+    http_code 200;
+    http_content "hello world\n";
+  }
+);
+  
 
 done_testing
